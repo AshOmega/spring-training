169473c1ef1f50bfa8ce1e22d1526d318d7dd110
karollil <karollil@adobe.com>
2020-04-07 07:47:35 +0530

    Use @Qualifier to distinguish beans

--- src/main/java/com/learning/controllers/AppControllers.java
+++ src/main/java/com/learning/controllers/AppControllers.java
@@ -1,5 +1,7 @@
 package com.learning.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 @ComponentScan
 public class AppControllers {
 
+    @Autowired
+    String sampleBean1;
+
+    @Autowired
+    @Qualifier("sampleBean2")  // use @Qualifier in case we want to create an object that doesnt have the same name as the bean name
+    String SampleBean2;
+
     @GetMapping("/")
     public String helloWorld(){
         return "Hello World";
+    }
+
+    @GetMapping("/samplebean1")
+    public String samplebean1(){
+        return sampleBean1;
+    }
+
+    @GetMapping("/samplebean2")
+    public String samplebean2(){
+        return SampleBean2;
     }
 }
